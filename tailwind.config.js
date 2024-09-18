428465480536f82a48b29af1b06259f06e91d736
Tomiwa Adeyemi <adeyemitomiwa7@gmail.com>
2024-09-19 00:16:14 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    screens: {
+      "ft": "900px",
+      'ss': '384px',
+      "ssm": '486px',
+      "sc": "405px",
+      ...defaultTheme.screens,
+    },
+    fontFamily: {
+    sans: ["Nunito", "sans-serif"],
+    logo: ['Germania One', 'cursive']
+    },
+      extend: {
+      gradientColorStops: {
+        'custom-gradient': 'rgba(2,0,36,1) 0%, rgba(9,9,121,1) 20%, rgba(62,53,67,1) 92%',
+      },
+    },
+  },
+  
+   plugins: [
+    require("tailwind-scrollbar-hide")
+  ],
+}
